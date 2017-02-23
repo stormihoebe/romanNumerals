22c2bc67caf8a9e51daea83372a37c2be9a56e8b
Stormi Hoebelheinrich and Xi Xia <xi@>
2017-02-23 11:34:15 -0800

    output string of numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 
 var romans = ["I", "V", "X", "L", "C", "D","M"];
 var breaknumbers = [1, 5, 10, 50, 100, 500, 1000];
+var outputArray = [];
+var outputString = "";
 
 var processing = function (number, indexOfBreak) {
   // found largest break point
@@ -9,6 +11,10 @@ var processing = function (number, indexOfBreak) {
   //how many breaknumbers was in our number?
   var howManyRoman = Math.floor(number/breaknumbers[indexOfBreak-1]);
   console.log(howManyRoman);
+  for (var j = 1; j <= howManyRoman; j++) {
+    outputArray.push(romans[indexOfBreak-1]);
+  };
+
 
   //wahts the remainder?
   var numberRemainder=(number%breaknumbers[indexOfBreak-1]);
@@ -18,13 +24,14 @@ var processing = function (number, indexOfBreak) {
 var numeralizeI = function(number) {
   for (var i=0;i<breaknumbers.length;i++) {
     if (number === breaknumbers[i]) {
-      return romans[i];
+      outputArray.push(romans[i]);
+      return;
     } else if (number < breaknumbers[i]) {
       processing (number, i);
       return;
     } else if (number > 1000) {
       processing (number, 7);
-      return;     
+      return;
     }
       else {
       }
@@ -37,12 +44,15 @@ var numeralizeI = function(number) {
 $(function(){
   $("#romanNumber").submit(function(){
     event.preventDefault();
-    var output = "";
+    outputString = "";
+    outputArray = [];
+
     var number = parseInt($("input#number").val());
 
 
-    output = numeralizeI(number);
-    console.log(output);
+    numeralizeI(number);
+    outputString = outputArray.join("");
+    console.log(outputString);
 
 
     // $(".output").text(output);
