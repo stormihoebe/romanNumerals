22a0ae72e6f1f17f54c0e7203d15223966014260
Alex Lee and Stormi Hoebelheinrich <alex@>
2017-02-22 17:02:19 -0800

    add style, fix capitalize bug, fixed README

--- README.md
+++ README.md
@@ -1,43 +1,52 @@
 
-# Project Title goes here
+# Pig Latin Translator
 
-#### Date and version goes here
+#### 2/22/2017
 
-#### By _Alex Lee_
+#### By _Alex Lee & Stormi Hoebelheinrich_
 
 ## Description
 
-Description goes here
-
+This is an application that takes in a sentence and returns a pig latin translation of that sentence.
+Specs:
+***The application should do noting to non-alphabetical characters***
+***The application should add "ay" to single-letter words beginning with a vowel***
+***the application should move consonants at the beginning of the word to the end of the word and then add "ay"
+to the end***
+***The application should move "qu" together when they appear at the beginning of a word***
+***The application should return the full sentence completely translated into pig latin***
 ## Setup/Installation Requirements
+Open the terminal and preform the following:
 ```
 cd /desiredLocation
 ```
 ```
-git clone https://github.com/doomcatlee/trackSuggester
+git clone https://github.com/doomcatlee/piglatin
 ```
 ```
-cd trackSuggester
+cd pigLatin
 ```
 ```
-open index.html
+open index.html in browser
 ```
 
 ## Known Bugs
 
-Known bugs here
+The program does not properly move q and u together to the end of the sentence. Punctuation was not considered, and sometimes appears in the middle of a word, before the "ay" ending.
 
 ## Support and contact details
 
-Contact information here
+Contact Stormi or Alex at epicodus if you need any support of have concerns
 
 ## Technologies Used
 
 _bash_
 _git_
+_js_
+
 
 ### License
 
 *This software is licensed under the MIT license.*
 
-Copyright (c) 2017 Alex Lee
+Copyright (c) 2017 Alex Lee & Stormi Hoebelheinrich
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,10 @@
-
-var vowels = ["a","i","e","o","u"];
-var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y", "z"];
-
-
-//1. Function to slice sentence into words***
-//2. function to check letters of a word, recognize when a vowel is found.***
-//3. Function to create pig latin. Slice word before the vowel, add that to the end of the word, then + ay to the end of the word.
-//4. pring the pig latin phrase to the page.
+var vowels = ["a","i","e","o","u","A","E","I","O","U"];
 var findVowel = function(word){
   var lettersArray = word.split("");
   var k = 0;
   for (var i = 0; i < lettersArray.length; i++) {
     for (var j = 0; j < vowels.length; j++) {
       if ((lettersArray[i] === vowels[j]) && (k === 0)) {
-        // console.log("we found a vowel!", i);
         k++;
         return i;
       };
@@ -35,90 +26,16 @@ var pigLatin = function(word) {
 };//end pigLatin function
 
 
-
-
 $(function(){
   $(".pigForm").submit(function(){
     event.preventDefault();
     var sentence = $("input#sentence").val();
     var words = sentence.split(" ");
-
     var output = "";
     words.forEach(function(word){
       output += pigLatin(word) + " ";
     });
     $(".output").text(output);
     console.log(output);
-
-
-
-
   });//submit
 });//jQuery
-
-
-
-
-
-//
-// var vowelBegin = function(word){
-//   // debugger;
-//   for (var i=0; i < vowels.length; i++){
-//       if (word.charAt(0) === vowels[i]){
-//         word = word + "ay";
-//         console.log(word);
-//       } else {
-//         if ((word.charAt(1) !== vowels[i]) && (word.charAt(2) === vowels[i])){
-//           console.log("word starts with 2 consonants")
-//           break;
-//         } else if (word.charAt(1) === vowels[i]){
-//           console.log("word starts with 1 consonant")
-//           break;
-//         } else if ((word.charAt(0) !== vowels[i]) && (word.charAt(1) !== vowels[i]) && (word.charAt(2) !== vowels[i])){
-//         console.log("word starts with 3 consonants")
-//         break;
-//       };
-//     };
-//     };
-//   };
-//
-//   var vowelBegin2 = function(word){
-//     debugger;
-//     for (var i=0; i < vowels.length; i++){
-//         if (word.charAt(0) === vowels[i]){
-//           word = word + "ay";
-//           console.log(word);
-//         } else{
-//           if (word.charAt(0) !== vowels[i]){
-//             if (word.charAt(1) !== vowels[i]){
-//               if (word.charAt(2) !== vowels[i]){
-//                 console.log("consonant 3")
-//               }
-//             }else{
-//               console.log("cosonant 2");
-//             }
-//           }else{
-//             console.log("consonant 1")
-//           }
-//         }
-//       }
-//     }
-//
-// //wordBreaker should return an array of letters from a word
-// var wordBreaker = function(word){
-//   var array = word.split("")
-//   return array;
-// };
-//
-//
-// $(function(){
-//   $(".pigForm").submit(function(){
-//     event.preventDefault();
-//     var sentence = $("input#sentence").val();
-//     var words = sentence.split(" ");
-//
-//     // console.log(vowelBegin2("ute"));
-//     // console.log(wordBreaker("dkaaa"))
-//
-//     var word = "stong";
-//     vowelBegin2(word);
