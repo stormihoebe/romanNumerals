f1ada5f4a4243f0d3f89063b9e2666f661d01d34
Stormi Hoebelheinrich and Xi Xia <stormi@>
2017-02-23 11:20:10 -0800

    refactor code to process number

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,28 @@
 var romans = ["I", "V", "X", "L", "C", "D","M"];
 var breaknumbers = [1, 5, 10, 50, 100, 500, 1000];
 
+var processing = function (number, indexOfBreak) {
+  // found largest break point
+  var largestRoman = romans[indexOfBreak-1];
+  console.log(largestRoman);
+  //how many breaknumbers was in our number?
+  var howManyRoman = Math.floor(number/breaknumbers[indexOfBreak-1]);
+  console.log(howManyRoman);
+
+  //wahts the remainder?
+  var numberRemainder=(number%breaknumbers[indexOfBreak-1]);
+  console.log(numberRemainder);
+};
+
 var numeralizeI = function(number) {
   for (var i=0;i<breaknumbers.length;i++) {
     if (number === breaknumbers[i]) {
       return romans[i];
     } else if (number < breaknumbers[i]) {
-      // found largest break point
-      var largestRoman = romans[i-1];
-      console.log(largestRoman);
-      //how many breaknumbers was in our number?
-      var howManyRoman = Math.floor(number/breaknumbers[i-1]);
-      console.log(howManyRoman);
-
-      //wahts the remainder?
-      var numberRemainder=(number%breaknumbers[i-1]);
-      console.log(numberRemainder);
-
+      processing (number, i);
       return;
     } else if (number > 1000) {
-      var largestRoman = romans[6];
-      console.log(largestRoman);
-      //how many breaknumbers was in our number?
-      var howManyRoman = Math.floor(number/1000);
-      console.log(howManyRoman);
-
-      //wahts the remainder?
-      var numberRemainder=(number%1000);
-      console.log(numberRemainder);
-
+      processing (number, 7);      
     }
       else {
       }
